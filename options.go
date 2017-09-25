6161856988af23705d8aaeaed3464f3496df07ba
Daniel Imfeld <daniel@danielimfeld.com>
2017-09-24 19:47:59 -1000

    Real options metadata endpoint

--- options.go
+++ options.go
@@ -2,19 +2,20 @@ package brokerage_server_ib
 
 import (
 	"context"
+	"sort"
 
 	"github.com/dimfeld/brokerage_server/types"
 	"github.com/dimfeld/ib"
 )
 
-func (p *IB) GetOptions(ctx context.Context, underlying string) (interface{}, error) {
+func (p *IB) GetOptionsChain(ctx context.Context, underlying string) (types.OptionChain, error) {
 
 	key := ContractKey{
 		Symbol: underlying,
 	}
 	contractDetails, err := p.contractManager.GetContractDetails(ctx, key)
 	if err != nil {
-		return nil, err
+		return types.OptionChain{}, err
 	}
 
 	p.LogDebugNormal("Got contract details", "data", contractDetails)
@@ -28,11 +29,25 @@ func (p *IB) GetOptions(ctx context.Context, underlying string) (interface{}, er
 		Exchange: "",
 	}
 
-	var output []*ib.SecurityDefinitionOptionParameter
+	expirations := map[string]bool{}
+	strikes := map[float64]bool{}
+	exchanges := []string{}
+	var multiplier string
+
 	err = p.syncMatchedRequest(ctx, request, func(r ib.Reply) (replyBehavior, error) {
 		switch data := r.(type) {
 		case *ib.SecurityDefinitionOptionParameter:
-			output = append(output, data)
+
+			for _, value := range data.Expirations {
+				expirations[value] = true
+			}
+
+			for _, value := range data.Strikes {
+				strikes[value] = true
+			}
+
+			exchanges = append(exchanges, data.Exchange)
+			multiplier = data.Multiplier // I think multiplier is always the same?
 			return REPLY_CONTINUE, nil
 		case *ib.SecurityDefinitionOptionParameterEnd:
 			return REPLY_DONE, nil
@@ -46,6 +61,26 @@ func (p *IB) GetOptions(ctx context.Context, underlying string) (interface{}, er
 
 		return REPLY_CONTINUE, nil
 	})
+
+	strikesList := make([]float64, 0, len(strikes))
+	for s := range strikes {
+		strikesList = append(strikesList, s)
+	}
+
+	expiresList := make([]string, 0, len(expirations))
+	for e := range expirations {
+		expiresList = append(expiresList, e)
+	}
+
+	sort.Strings(expiresList)
+	sort.Float64s(strikesList)
+
+	output := types.OptionChain{
+		Underlying:  underlying,
+		Multiplier:  multiplier,
+		Strikes:     strikesList,
+		Expirations: expiresList,
+	}
 
 	return output, err
 }
